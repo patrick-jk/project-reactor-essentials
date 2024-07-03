1d34edb39ce8d9216d3dd89feea1bbccbc91aee3
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-07-03 13:14:59 -0300

    Mono doOn and onError methods

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -94,12 +94,79 @@ public class MonoTest {
         mono.subscribe(s -> log.info("Value {}", s),
                 Throwable::printStackTrace,
                 () -> log.info("FINISHED!"),
-                Subscription::cancel);
+                subscription -> subscription.request(5));
 
         log.info("---------------------------------");
 
         StepVerifier.create(mono)
                 .expectNext(name.toUpperCase())
                 .verifyComplete();
+    }
+
+    @Test
+    public void monoDoOnMethods() {
+        String name = "DevDojo Academy";
+        Mono<Object> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase)
+                .doOnSubscribe(subscription -> log.info("Subscribed"))
+                .doOnRequest(longNumber -> log.info("Request Received, starting doing something..."))
+                .doOnNext(s -> log.info("Value is here. Executing doOnNext {}", s))
+                .flatMap(s -> Mono.empty())
+                .doOnNext(s -> log.info("Value is here. Executing doOnNext {}", s))//will not be executed
+                .doOnSuccess(s -> log.info("doOnSuccess executed {}", s));
+
+        mono.subscribe(s -> log.info("Value {}", s), Throwable::printStackTrace, () -> log.info("FINISHED!"));
+    }
+
+    @Test
+    public void monoDoOnError() {
+        Mono<Object> error = Mono.error(new IllegalArgumentException("Illegal argument exception"))
+                .doOnError(e -> log.error("Error message: {}", e.getMessage()))
+                .doOnNext(s -> log.info("Executing this doOnNext"))//will not be executed
+                .log();
+
+
+        StepVerifier.create(error)
+                .expectError(IllegalArgumentException.class)
+                .verify();
+
+    }
+
+    @Test
+    public void monoOnErrorResume() {
+        String name = "DevDojo Academy";
+        Mono<Object> error = Mono.error(new IllegalArgumentException("Illegal argument exception"))
+                .onErrorResume(s -> {
+                    log.info("Inside On Error Resume");
+                    return Mono.just(name);
+                })
+                .doOnError(e -> log.error("Error message: {}", e.getMessage()))
+                .log();
+
+
+        StepVerifier.create(error)
+                .expectNext(name)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void monoOnErrorReturn() {
+        String name = "DevDojo Academy";
+        Mono<Object> error = Mono.error(new IllegalArgumentException("Illegal argument exception"))
+                .onErrorReturn("EMPTY")
+                .onErrorResume(s -> {
+                    log.info("Inside On Error Resume");
+                    return Mono.just(name);
+                })
+                .doOnError(e -> log.error("Error message: {}", e.getMessage()))
+                .log();
+
+
+        StepVerifier.create(error)
+                .expectNext("EMPTY")
+                .verifyComplete();
+
     }
 }
