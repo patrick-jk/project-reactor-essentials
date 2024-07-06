57677b31d7734387ae468fc025394c288f9f0040
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-07-06 09:29:55 -0300

    limitRate and ConnectableFlux

--- src/test/java/academy/devdojo/reactive/test/FluxTest.java
+++ src/test/java/academy/devdojo/reactive/test/FluxTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -146,6 +147,20 @@ public class FluxTest {
     }
 
     @Test
+    public void fluxSubscriberPrettyBackpressure() {
+        Flux<Integer> flux = Flux.range(1, 10)
+                .log()
+                .limitRate(3);
+
+        flux.subscribe(i -> log.info("Number {}", i));
+
+        log.info("--------------------------------------");
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .verifyComplete();
+    }
+
+    @Test
     public void fluxSubscriberIntervalOne() throws InterruptedException {
         Flux<Long> interval = Flux.interval(Duration.ofMillis(100))
                 .take(10)
@@ -172,5 +187,51 @@ public class FluxTest {
     private Flux<Long> createInterval() {
         return Flux.interval(Duration.ofDays(1))
                 .log();
+    }
+
+    @Test
+    public void connectableFlux() throws InterruptedException {
+        ConnectableFlux<Integer> connectableFlux = Flux.range(1, 10)
+                .log()
+                .delayElements(Duration.ofMillis(100))
+                .publish();
+
+//        connectableFlux.connect();
+//
+//        log.info("Thread sleeping for 300ms");
+//
+//        Thread.sleep(300);
+//
+//        connectableFlux.subscribe(i -> log.info("Sub1 number {}", i));
+//
+//        log.info("Thread sleeping for 200ms");
+//
+//        Thread.sleep(200);
+//
+//        connectableFlux.subscribe(i -> log.info("Sub2 number {}", i));
+
+        StepVerifier
+                .create(connectableFlux)
+                .then(connectableFlux::connect)
+                .thenConsumeWhile(i -> i <= 5)
+                .expectNext(6, 7, 8, 9, 10)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void connectableFluxAutoConnect() {
+        Flux<Integer> fluxAutoConnect = Flux.range(1, 5)
+                .log()
+                .delayElements(Duration.ofMillis(100))
+                .publish()
+                .autoConnect(2);
+
+        StepVerifier
+                .create(fluxAutoConnect)
+                .then(fluxAutoConnect::subscribe)
+                .expectNext(1,2,3,4,5)
+                .expectComplete()
+                .verify();
     }
 }
