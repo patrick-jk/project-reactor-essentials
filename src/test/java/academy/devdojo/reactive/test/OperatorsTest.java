c369981b6a2785d9a2a0e9a2b9eb7c39a30fa44b
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-07-07 13:49:23 -0300

    fromCallable and operators

--- src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
+++ src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
@@ -1,10 +1,18 @@
 package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 public class OperatorsTest {
@@ -125,5 +133,137 @@ public class OperatorsTest {
                 .expectSubscription()
                 .expectNext(1, 2, 3, 4)
                 .verifyComplete();
+    }
+
+    @Test
+    public void subscribeOnIO() {
+        Mono<List<String>> list = Mono.fromCallable(() -> Files.readAllLines(Path.of("text-file")))
+                .log()
+                .subscribeOn(Schedulers.boundedElastic());
+
+        list.subscribe(s -> log.info("{}", s));
+
+        StepVerifier.create(list)
+                .expectSubscription()
+                .thenConsumeWhile(l -> {
+                    Assertions.assertFalse(l.isEmpty());
+                    log.info("Size {}", l.size());
+                    return true;
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    public void switchIfEmptyOperator() {
+        Flux<Object> flux = emptyFlux()
+                .switchIfEmpty(Flux.just("not empty anymore"))
+                .log();
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext("not empty anymore")
+                .expectComplete()
+                .verify();
+    }
+
+    private Flux<Object> emptyFlux() {
+        return Flux.empty();
+    }
+
+    @Test
+    public void deferOperator() throws Exception {
+        Mono<Long> just = Mono.just(System.currentTimeMillis());
+        Mono<Long> defer = Mono.defer(() -> Mono.just(System.currentTimeMillis()));
+
+        defer.subscribe(l -> log.info("time {}", l));
+        Thread.sleep(100);
+        defer.subscribe(l -> log.info("time {}", l));
+        Thread.sleep(100);
+        defer.subscribe(l -> log.info("time {}", l));
+        Thread.sleep(100);
+        defer.subscribe(l -> log.info("time {}", l));
+
+        AtomicLong atomicLong = new AtomicLong();
+        defer.subscribe(atomicLong::set);
+        Assertions.assertTrue(atomicLong.get() > 0);
+    }
+
+    @Test
+    public void concatOperator() {
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> concatFlux = Flux.concat(flux1, flux2).log();
+
+        StepVerifier.create(concatFlux)
+                .expectSubscription()
+                .expectNext("a", "b", "c", "d")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void concatWithOperator() {
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+//        Flux<String> concatFlux = flux1.concatWith(flux2).log();
+
+        StepVerifier.create(flux1.concatWith(flux2).log())
+                .expectSubscription()
+                .expectNext("a", "b", "c", "d")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void combineLatestOperator() {
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> combineLatest = Flux.combineLatest(flux1, flux2, (s1, s2) -> s1.toUpperCase() + s2.toUpperCase())
+                .log();
+
+        StepVerifier.create(combineLatest)
+                .expectSubscription()
+                .expectNext("BC", "BD")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void mergeOperator() throws InterruptedException {
+        Flux<String> flux1 = Flux.just("a", "b").delayElements(Duration.ofMillis(200));
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeFlux = Flux.merge(flux1, flux2)
+                .delayElements(Duration.ofMillis(200))
+                .log();
+
+//        mergeFlux.subscribe(log::info);
+
+//        Thread.sleep(1000);
+
+        StepVerifier.create(mergeFlux)
+                .expectSubscription()
+                .expectNext("c", "d", "a", "b")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void mergeWithOperator() {
+        Flux<String> flux1 = Flux.just("a", "b").delayElements(Duration.ofMillis(200));
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeFlux = flux1.mergeWith(flux2)
+                .delayElements(Duration.ofMillis(200))
+                .log();
+
+        StepVerifier.create(mergeFlux)
+                .expectSubscription()
+                .expectNext("c", "d", "a", "b")
+                .expectComplete()
+                .verify();
     }
 }
